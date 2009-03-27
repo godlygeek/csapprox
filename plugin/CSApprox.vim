godlygeek/csapprox

commit 4091aca090cfa7938c5e07a81c5600776eec0614

Fix a bug dumping colors not set in the scheme
CSApproxSnapshot should make a snapshot of the currently used colors,
rather than the colors defined in the colorscheme.  But, when
CSApproxSnapshot() changed &t_Co, the scheme was reloaded, causing :hi
clear to run and delete all colors.  Definitely not desired.  Stop that
from happening in the same way as it is prevented in CSApprox().

plugin/CSApprox.vim
@@ -759,6 +759,18 @@ function! s:CSApproxSnapshot(file, overwrite)
     let save_CSApprox_eterm = g:CSApprox_eterm
   endif
 
+  " Needed just like in CSApprox()
+  if exists("g:colors_name")
+    let colors_name = g:colors_name
+    unlet g:colors_name
+  endif
+
+  " Needed just like in CSApprox()
+  if exists("g:syntax_cmd")
+    let syntax_cmd = g:syntax_cmd
+  endif
+  let g:syntax_cmd = "PLEASE DON'T CHANGE ANY COLORS!!!"
+
   try
     let lines = []
     let lines += [ '" This scheme was created by CSApproxSnapshot' ]
@@ -840,6 +852,15 @@ function! s:CSApproxSnapshot(file, overwrite)
     endif
     if exists("save_CSApprox_eterm")
       let g:CSApprox_eterm = save_CSApprox_eterm
+    endif
+
+    if exists("colors_name")
+      let g:colors_name = colors_name
+    endif
+
+    unlet g:syntax_cmd
+    if exists("syntax_cmd")
+      let g:syntax_cmd = syntax_cmd
     endif
   endtry
 endfunction

commit 4eb86232c68d03b164575878a193579035c2a4c2

Fix major regression in CSApproxSnapshot
After 4091aca090cfa7938c5e07a81c5600776eec0614 dumping colors with
CSApproxSnapshot was completely broken.  That commit stopped changing
&t_Co from reverting changes to colors not made by the colorscheme by
preventing the colorscheme from being re-sourced every time &t_Co
changed.  Unfortunately, that stopped the ColorScheme autocmd from
firing, which stopped s:CSApprox() from calculating the colors, which
meant we would dump the same colors regardless of &t_Co, and regardless
of {xterm,konsole,eterm}.

Fix this problem by calling s:CSApprox() explicitly after changing &t_Co

plugin/CSApprox.vim
@@ -796,6 +796,7 @@ function! s:CSApproxSnapshot(file, overwrite)
         set t_Co=256
       endif
 
+      call s:CSApprox()
       let highlights = s:Highlights()
       call s:FixupGuiInfo(highlights)
 